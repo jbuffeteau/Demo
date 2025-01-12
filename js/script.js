e745b58f734cb96c48123a487722dae525af18fe
jbuffeteau <39774336+jbuffeteau@users.noreply.github.com>
2025-01-12 17:43:19 +0100

    Batch update

---
+++ js/script.js
@@ -0,0 +1,4 @@
+
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":1199},"pages":[{"id":"page-page-1","displayName":"Page 1","link":{"linkType":"LinkTypePage","href":"#!page-page-1"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-pageexo1","displayName":"PageExo1","link":{"linkType":"LinkTypePage","href":"#!page-pageexo1"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false}]}
